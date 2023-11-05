4b1d0e85324421a27065dbb55af8b1432b5741a4
Muinul-Islam <muinul1022@gmail.com>
2023-11-05 22:53:35 +0600

    Set up the routing and created navbar and footer with responsive

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Shared/Footer";
+import NavBar from "../Shared/NavBar";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default MainLayout;
---
+++ src/Shared/Footer.jsx
@@ -0,0 +1,39 @@
+import logo from "../assets/img/logo.png";
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer p-10 bg-green-700 text-white max-w-screen-xl mx-auto rounded-xl">
+        <aside>
+          <img className="w-20 rounded-full" src={logo} alt="" />
+          <p>
+            Community Food Sharing
+            <br />
+            Providing Food For The People
+          </p>
+        </aside>
+        <nav>
+          <header className="footer-title">Services</header>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </nav>
+        <nav>
+          <header className="footer-title">Company</header>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </nav>
+        <nav>
+          <header className="footer-title">Legal</header>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </nav>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
